15ee1d6a14cd8ac68590da3c8f70742f2aae349e
Khoirul U <139238214+umamkhoirul783@users.noreply.github.com>
2023-12-13 20:55:34 +0700

    Add files via upload

--- classfinder/map/index.php
+++ classfinder/map/index.php
@@ -1,33 +1,9 @@
-<?php
-session_start();
-
-// Pastikan hanya user yang bisa mengakses halaman ini
-if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'user') {
-    header("location: ../login.php");
-    exit();
-}
-
-// Ambil informasi pengguna dari sesi
-$user_id = $_SESSION['user_id'];
-$username = $_SESSION['username'];
-$role = $_SESSION['role'];
-
-$roomData = array(
-    array('id' => 1, 'name' => 'Room 101', 'capacity' => 20, 'position' => array(100, 200)),
-    array('id' => 2, 'name' => 'Room 102', 'capacity' => 15, 'position' => array(300, 150)),
-  // Tambahkan data ruangan lain sesuai kebutuhan
-);
-
-// Convert PHP array to JSON for use in JavaScript
-$roomDataJson = json_encode($roomData);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="style.css">
     <!-- Sertakan Leaflet CSS -->
     <!-- <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" /> -->
     <title>Room Map - ClassFinder</title>
@@ -42,57 +18,187 @@ $roomDataJson = json_encode($roomData);
     </style> -->
 </head>
 <body>
-    <div class="container">
+    <div class="container" id="container">
         <div class="navbar">
-            <h2>Welcome, <?php echo $username; ?> (User)</h2>
-        </div>
+        <h1>Class<span>Finder</span></h1>
+            <ul>
+                <li><a href="../user/dashboard.php">Overview</a></li>
+                <li><a href="index.php">Classes</a></li>
+                <li><a href="../notifications/notif_user.php">Notifications</a></li>
+                <li><a href="../logout.php">Logout</a></li>
+            </ul>
+        </div>  
 
-        <div class="sidebar">
-            <a href="../user/dashboard.php">Overview</a>
-            <a href="index.php">Map</a>
-            <a href="../rooms/room_info.php">Room Information</a>
-            <!-- <a href="booking.php">Book a Room</a> -->
-            <!-- <a href="booking_status.php">Booking Status</a> -->
-            <a href="../notifications/notif_user.php">Notifications</a>
-            <!-- <a href="profile.php">Profile</a> -->
-            <a href="../logout.php">Logout</a>
-        </div>
-
-        <div class="content">
-            <!-- Konten untuk tampilan peta dan informasi ruangan -->
-            <h2>Room Map - E11 Building</h2>
-            <!-- Sertakan Leaflet JavaScript -->
-            <!-- <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script> -->
-            <div id="map"></div>
-
-            <script>
-                document.addEventListener("DOMContentLoaded", function () {
-                    // Inisialisasi peta Leaflet
-                    var map = L.map('map').setView([0, 0], 1);
-
-                    // Load the tile layer from OpenStreetMap
-                    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    // }).addTo(map);
-
-                    // Room data dari PHP ke JavaScript
-                    var roomData = <?php echo $roomDataJson; ?>;
-
-                    // Tambahkan marker untuk setiap ruangan
-                    roomData.forEach(function (room) {
-                        var marker = L.marker(room.position)
-                            .addTo(map)
-                            .bindPopup('<b>' + room.name + '</b><br>Capacity: ' + room.capacity + ' people');
-                        
-                        // Tambahkan event klik ke marker
-                        marker.on('click', function () {
-                            // Implementasikan logika untuk menampilkan detail ruangan atau melakukan permintaan ruangan
-                            alert('Room ' + room.name + ' clicked!');
-                        });
-                    });
-                });
-            </script>
-        </div>
+        <!--OVERVIEW section start -->
+        <section class="overview" id="overview">
+            <img src="https://www.clipartkey.com/mpngs/m/118-1188761_avatar-cartoon-profile-picture-png.png" alt="Avatar" class="avatar">
+            <main class="profile">
+                <h3>Welcome<span> yourname</span></h3>
+                <p>5302422030</p>
+                <p>Informatics and Computer Engineering Education</p>
+            </main>
+        </section>
+        <!--OVERVIEW section end -->
+        <!--Classes section start -->
+        <section class="classes" id="classes">
+            <h2>Classrooms</h2>
+        </section>
+        <main class="rooms">
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room1">
+                        <li class="status">Available</li>
+                    </div>
+                    <div class="content">
+                        <h3>E11-109</h3>
+                        <p>Computer Lab</p>
+                        <p>Capacity: 30</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room2">
+                        <li class="status">Available</li>
+                    </div>
+                    <div class="content">
+                        <h3>E11-111</h3>
+                        <p>Classroom</p>
+                        <p>Capacity: 40</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room3">
+                    <li class="booked">Booked</li>
+                    </div>
+                    <div class="content">
+                        <h3>E11-112</h3>
+                        <p>Classroom</p>
+                        <p>Capacity: 35</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room4">
+                    </div>
+                    <div class="content">
+                        <h3>E11-201</h3>
+                        <p>Computer Lab</p>
+                        <p>Capacity: 20</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room5">
+                    </div>
+                    <div class="content">
+                        <h3>E11-202</h3>
+                        <p>Computer Lab</p>
+                        <p>Capacity: 25</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room5">
+                    </div>
+                    <div class="content">
+                        <h3>E11-205</h3>
+                        <p>Classroom</p>
+                        <p>Capacity: 35</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room5">
+                    </div>
+                    <div class="content">
+                        <h3>E11-206</h3>
+                        <p>Classroom</p>
+                        <p>Capacity: 35</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room5">
+                    </div>
+                    <div class="content">
+                        <h3>E11-207</h3>
+                        <p>Classroom</p>
+                        <p>Capacity: 35</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room5">
+                    </div>
+                    <div class="content">
+                        <h3>E11-209</h3>
+                        <p>Classroom</p>
+                        <p>Capacity: 35</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room5">
+                    </div>
+                    <div class="content">
+                        <h3>E11-210</h3>
+                        <p>Classroom</p>
+                        <p>Capacity: 40</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room5">
+                    </div>
+                    <div class="content">
+                        <h3>E11-301</h3>
+                        <p>Computer Lab</p>
+                        <p>Capacity: 25</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room5">
+                    </div>
+                    <div class="content">
+                        <h3>E11-302</h3>
+                        <p>Computer Lab</p>
+                        <p>Capacity: 30</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room5">
+                    </div>
+                    <div class="content">
+                        <h3>E11-305</h3>
+                        <p>Classroom</p>
+                        <p>Capacity: 35</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room5">
+                    </div>
+                    <div class="content">
+                        <h3>E11-306</h3>
+                        <p>Computer Lab</p>
+                        <p>Capacity: 25</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                    <img src="https://2.bp.blogspot.com/-2v5DE5vy6y4/UZzaEAcy2HI/AAAAAAAAACU/WvleJTIxYUI/s1600/photo.JPG" alt="room5">
+                    </div>
+                    <div class="content">
+                        <h3>E11-307</h3>
+                        <p>Classroom</p>
+                        <p>Capacity: 30</p>
+                    </div>
+                </div>
+        </main>
+        <!--Classes section end -->
     </div>
 </body>
 </html>
